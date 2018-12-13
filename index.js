fa1c2eddd7f14238aeeceb9c8cd95a04c8804520
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-12-13 10:19:40 -0800

    Fix scrollspy

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-$('main').scrollspy({ target: '#mainNav' })
+function scroll() {
+    $('body').scrollspy({ target: '#mainNav' })
+}
 
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
+function collapseNav() {
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+    
+    $('.navbar-brand').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+    
+    $('body').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+}
 
-$('.navbar-brand').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
-
-$('body').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
-
-$('#navbarNav .navbar-nav a').on( 'click', function () {
-	$( '#navbarNav .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-	$( this ).parent( 'li' ).addClass( 'active' );
-});
+function handleCreateApp() {
+    scroll();
+    collapseNav();
+}
+$(handleCreateApp);
